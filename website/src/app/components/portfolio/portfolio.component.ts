99392e30ec821654c56e79ffb8739344d8132f0b
Paul <labellepaul@outlook.com>
2020-01-30 11:32:40 -0500

    moved about me and fixed links on home page

--- website/src/app/components/portfolio/portfolio.component.ts
+++ website/src/app/components/portfolio/portfolio.component.ts
@@ -7,26 +7,12 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss'],
-  animations: [
-    trigger('list', [
-      transition(':enter', [
-        query('@items', stagger(60, animateChild()))
-      ])
-    ]),
-    trigger('items', [
-      transition(':enter', [
-        style({ transform: 'scale(0)', opacity: 0 }),
-        animate('0.350s cubic-bezier(.8, -0.6, 0.2, 1.5)',
-          style({ transform: 'scale(1)', opacity: 1 }))
-      ])
-    ])
-  ]
+  styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
 
   types: string[];
-  sourceNames = {TypeScript: 'Angular', HTML: 'HTML5, CSS', 'C#': 'Asp.Net' };
+  sourceNames = {TypeScript: 'Angular', HTML: 'HTML5', 'C#': 'Asp.Net' };
 
   private _selectedType = 'All';
 
